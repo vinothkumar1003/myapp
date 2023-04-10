4115952fe782ecff1555649b9721c1e5ad70d971
vinothkumar1003 <128831424+vinothkumar1003@users.noreply.github.com>
2023-04-10 06:38:32 +0530

    adde  cum

---
+++ src/Cum.js
@@ -0,0 +1,17 @@
+function Add(props)
+{
+            return <h1>my name is {props.fname} {props.lname}</h1>
+              
+}                
+
+function Cum()
+{
+   return(
+    <div>
+    <h1>good morning</h1>
+    <Add fname="vinoth" lname="kumar"/>
+    </div>
+
+   );
+}
+        export default Cum;
